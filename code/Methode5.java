395231b88e62c7e84264b3b5273e129cdfab910f
RoxaneC <61699049+RoxaneC@users.noreply.github.com>
2020-04-23 13:31:09 +0200

    Update Methode5.java

--- code/Methode5.java
+++ code/Methode5.java
@@ -79,39 +79,6 @@ public class Methode5 {
 		return binaire;
 	}	
 	
-	/**
-	 * Normalise une image
-	 * 
-	 * @param img L'image à normaliser
-	 * @return L'image normalisée en niveau de gris
-	 * 
-	 * @author Juliette/Nicolas
-	 */
-	public static BufferedImage normalisation(BufferedImage img) {
-		/*
-		 * Transforme l'image en niveau de gris
-		 */
-		niveauGris(img);
-		
-		int largeur = img.getWidth();
-		int longueur = img.getHeight();
-		
-		BufferedImage resultat = new BufferedImage(largeur, longueur, BufferedImage.TYPE_4BYTE_ABGR);
-		
-		int[] hist = Histogrammes.histogrammeGris(img);
-		int x = minMod(hist);
-		int y = maxMod(hist);
-		
-		for(int colonne = 0; colonne<largeur ; colonne++) {
-			for(int ligne = 0 ; ligne<longueur ; ligne++) {
-				int p = img.getRGB(colonne, ligne);
-				resultat.setRGB(colonne, ligne, (255*(p-x)/(y-x)));
-			}
-		}
-		
-		return resultat;
-	}
-		
 	
 	//METHODES PRIVÉES UTILISÉES
 	/**
@@ -160,95 +127,18 @@ public class Methode5 {
 		return iMax;
 	}
 	
-	/**
-	 * Permet de transformer une image couleur en image de niveaux de gris
-	 * 
-	 * @param img L'image à modifier
-	 */
-	private static void niveauGris(BufferedImage img) {
-		int largeur = img.getWidth();
-		int longueur = img.getHeight();
-		
-		/*
-		 * Parcours de l'image
-		 */
-		for(int colonne = 0 ; colonne < largeur ; colonne++) {
-			for(int ligne = 0 ; ligne < longueur ; ligne++) {
-				int p = img.getRGB(colonne, ligne);
-				
-				/*
-				 * Récuperation des valeurs RGB
-				 */
-				int rouge = (p >> 16) & 0xff;
-				int vert = (p >> 8) & 0xff;
-				int bleu = p  & 0xff;
-				
-				int a = (rouge+vert+bleu)/3;
-				
-				/*
-				 * Reaffectation comme niveau de gris
-				 */
-				img.setRGB(colonne, ligne, (0xff<<24) | (a<<16) | (a<<8) | a);
-			}
-		}
-	}
-	
-	/**
-	 * Trouve l'indice de la première valeur différente de 0 d'un tableau
-	 * 
-	 * @param liste La liste à étudier
-	 * @return L'indice de la première valeur différente de 0
-	 */
-	private static int minMod(int[] tab) {
-		int i = 0;
-		/*
-		 * On parcourt la liste dans l'ordre croissant
-		 */
-		while (tab[i] == 0) {
-			i++;
-		}
-		
-		return i; 
-	}
-	
-	/**
-	 * Trouve l'indice de la dernière valeur différente de 0 d'un tableau
-	 * 
-	 * @param liste La liste à étudier
-	 * @return L'indice de la dernière valeur différente de 0
-	 */
-	private static int maxMod(int[] tab) {
-		int i = tab.length - 1;
-		/*
-		 * On parcourt la liste dans l'ordre décroissant
-		 */
-		while (tab[i] == 0) {
-			i--;
-		}
-		
-		return i; 
-	}
-	
-	
 	
 	
 	public static void main(String[] args) {
 		
 		// Mettre le path complet de l'image à étudier /!\
-		BufferedImage imgBase = Internet.rotateClockwise90(Internet.chargerImage("/bdd/escaliers_droits_2.jpg"));	//---> CHARGER : DONE OK
+		BufferedImage imgBase = Internet.rotateClockwise90(Internet.chargerImage("/home/roxane/eclipse-workspace/ImageProjet/bdd/escaliers_droits_1.jpg"));	//---> CHARGER : DONE OK
 
-		imgBase = normalisation(imgBase);						//---> NORMALISATION : DONE NOt?OK
 		imgBase = Filtres.filtreSobelY(imgBase);				//---> SOBEL 3*3 : DONE OK
 		imgBase = binarisationOtsu(imgBase);					//---> BINARISATION OTSU : DONE OK
-		imgBase = Filtres.flouGaussien(imgBase);				//---> GAUSSIEN (5*5 pour l'instant) : DONE OK		
-		imgBase = Filtres.filtreMedian(imgBase);				//---> MEDIAN 3*3 : DONE OK		
+		imgBase = Filtres.flouGaussien(imgBase);				//---> GAUSSIEN (5*5 pour l'instant) : DONE OK
+		imgBase = Filtres.filtreMedian(imgBase);				//---> MEDIAN 3*3 : DONE OK	
 		imgBase = Filtres.fermeture(imgBase);					//---> FERMETURE 7*7 : DONE OK
-		
-		int[] histProj = Histogrammes.histogrammeProjete(imgBase);				//---> HISTOGRAMME PROJETÉ : DONE OK
-		int resultat = Histogrammes.compterMarches(histProj, imgBase);			//---> CALCUL NB MARCHES : DONE ?
-		
-		System.out.print(resultat);
-		
 		
 		try {
 			Internet.afficherImage(imgBase);
@@ -256,7 +146,14 @@ public class Methode5 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-				
+		
+		int[] histProj = Histogrammes.histogrammeProjete(imgBase);				//---> HISTOGRAMME PROJETÉ : DONE OK
+		int resultat = Histogrammes.compterMarches(histProj, imgBase);			//---> CALCUL NB MARCHES : DONE ?
+		
+		imgBase.flush();
+		
+		System.out.print("Nombre de marches : " + resultat);
+						
 	}
 	
 }
